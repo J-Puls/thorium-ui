9c4d3e68d338b2681d1f5002ef914eab0865fd8a
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-09-17 11:36:10 -0400

    Debug disabled "link" button variant
    - Disabled buttons now have reduced text opacity

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import customStyles from './customStyles';
 import customThemes from './customThemes';
-import { ThoriumRoot, Block, Layer, Button, ThemeToggle } from 'thorium-ui';
+import { ThoriumRoot, Block, Layer, ThemeToggle } from 'thorium-ui';
 
 const App = () => {
   return (
     <ThoriumRoot customStyles={customStyles} customThemes={customThemes}>
       <Layer justify='center'>
-        <Block all={3}>
+        <Block all={12} justify='center'>
           <ThemeToggle size='lg'></ThemeToggle>
         </Block>
-        <Block all={12}>
-        <Button variant="secondary">Normal</Button>
-          <Button isDisabled variant="secondary">Disabled</Button>
-        </Block>
       </Layer>
+      <Layer justify='center'></Layer>
     </ThoriumRoot>
   );
 };
