cf94d70502931abeb3b0e4fc36f00a7d1387ed6a
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-10-03 14:00:47 -0400

    Convert to forwardRef & add removeMessage functionality
    - MessageBox is now a forwardRef
    - Existing Messages are now controlled via MessageBoxContext

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,30 +1,44 @@
-import React, { Component, createRef } from 'react';
+/* React */
+import React, { forwardRef, useState } from 'react';
+/* Thorium-UI */
 import { Layer } from './Layer';
+/* Context */
 import { MessageBoxProvider } from '../context/MessageBoxContext';
 
-class MessageBox extends Component {
-  constructor() {
-    super();
-    this.messageBox = createRef();
-  }
+const MessageBox = forwardRef(function ThMessageBox(props, ref) {
+  const children = new Set();
+  props.children.forEach((child, ind) => {
+    children.add(React.cloneElement(child, { messageID: ind, key: ind }));
+  });
 
-  componentDidMount() {
-    console.log(this.messageBox.current);
-  }
-  render() {
-    return (
-      <MessageBoxProvider value={{ parent: this.messageBox.current }}>
-        <Layer
-          vertical
-          data-testid='message-box'
-          style={{ margin: '15px' }}
-          ref={this.messageBox}
-        >
-          {this.props.children}
-        </Layer>
-      </MessageBoxProvider>
-    );
-  }
-}
+  const [messages, setMessages] = useState(children);
+
+  const removeMessage = (ID) => {
+    let newMessages = new Set(messages);
+    for (let message of messages) {
+      console.log(message.props.messageID, ID);
+      message.props.messageID === ID && newMessages.delete(message);
+    }
+    setMessages(newMessages);
+  };
+
+  return (
+    <MessageBoxProvider
+      value={{
+        messages,
+        removeMessage: (ID) => removeMessage(ID)
+      }}
+    >
+      <Layer
+        vertical
+        data-testid='message-box'
+        style={{ margin: '15px' }}
+        ref={ref}
+      >
+        {messages}
+      </Layer>
+    </MessageBoxProvider>
+  );
+});
 
 export default MessageBox;
