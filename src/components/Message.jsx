a7cf9429b644a141aba555067d2a0d1eaaabcede
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-10-03 14:02:14 -0400

    Added styling & state functionality
    - Debugged padding conflict
    - Added multi-stage transition when dismissing message
    - Messages now remove themselves from MessageBoxContext once dismissed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,42 +1,69 @@
-import React, { useState, useContext } from 'react';
+/* React */
+import React, {
+  useState,
+  useLayoutEffect,
+  forwardRef,
+  useContext
+} from 'react';
+/* Thorium-UI */
 import { Block } from './Block';
+/* Style */
 import { messageStyle } from '../styles/messageStyle';
+/* Hooks */
 import { useTheme } from '../utils/hooks/useTheme';
-// import { sleep } from '../utils/sleep';
+/* Utils */
+import { sleep } from '../utils/sleep';
+/* Context */
 import { MessageBoxContext } from '../context/MessageBoxContext';
 
-export const Message = (props) => {
-  // const parent = useContext(MessageBoxContext).parent;
-  let thisMessage;
+export const Message = forwardRef(function ThMessage(props, ref) {
+  const removeMessage = useContext(MessageBoxContext).removeMessage;
   const theme = useTheme().message[props.variant];
   const [isVisible, setIsVisible] = useState(true);
-  const handleClose = async () => {
+  const [isDismissed, setIsDismissed] = useState(false);
+  const [renderStyle, setRenderStyle] = useState({
+    ...messageStyle.general,
+    ...messageStyle.visible,
+    ...theme,
+    ...props.style
+  });
+
+  const handleDismiss = async () => {
     setIsVisible(false);
-    // await sleep(350);
-    // console.log(parent.props.children.delete(thisMessage));
+    await sleep(300);
+    setIsDismissed(true);
+    removeMessage(props.messageID);
   };
 
-  const renderStyle = isVisible
-    ? { ...messageStyle.general, ...messageStyle.visible }
-    : { ...messageStyle.general, ...messageStyle.hidden };
+  useLayoutEffect(() => {
+    isVisible && setRenderStyle({ ...renderStyle, ...theme });
+  }, [theme, isVisible]);
+  useLayoutEffect(() => {
+    !isVisible &&
+      setRenderStyle({ ...messageStyle.general, ...messageStyle.hidden });
+  }, [isVisible]);
+
+  useLayoutEffect(() => {
+    isDismissed && setRenderStyle({ ...messageStyle.dismissed });
+  }, [isDismissed]);
 
   return (
     <Block
       data-testid='message'
       all={12}
-      style={{ ...renderStyle, ...theme, ...props.style }}
+      style={renderStyle}
       justify='center'
-      ref={(el) => (thisMessage = el)}
+      ref={ref}
     >
       <span style={{ paddingRight: '1rem' }}>{props.children}</span>
       <button
-        onClick={handleClose}
+        onClick={handleDismiss}
         style={{ ...messageStyle.button, color: theme.color }}
       >
         &#128473;
       </button>
     </Block>
   );
-};
+});
 
 export default Message;
