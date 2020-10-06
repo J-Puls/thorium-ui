a6621cc34eb20233b1bf13bfd579ff1a29963145
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-10-06 12:48:13 -0400

    Modify formatting and data-testid

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,12 @@
 /* React */
-import React from 'react';
+import React from "react";
 /* Style */
-import { containerStyle } from '../styles/containerStyle';
+import { containerStyle } from "../styles/containerStyle";
 /* Utils */
-import mapPropsToAttrs from '../utils/mapPropsToAttrs';
+import mapPropsToAttrs from "../utils/mapPropsToAttrs";
 /* Breakpoint configuration */
-import { config } from '../config';
-import { useViewportSizeName } from '../hooks/thoriumRoot/useViewportSizeName';
+import { config } from "../config";
+import { useViewportSizeName } from "../hooks/thoriumRoot/useViewportSizeName";
 
 /**
  * A responsive, centered and padded wrapper for page contents.
@@ -17,11 +17,11 @@ export const Container = (props) => {
 
   return (
     <div
-      data-testid='container'
+      data-testid="th-container"
       {...mapPropsToAttrs(props)}
       style={{
         ...containerStyle,
-        maxWidth: vpWidth / config.containerSizes[vpSizeName] || '100%',
+        maxWidth: vpWidth / config.containerSizes[vpSizeName] || "100%",
         ...props.style
       }}
     >
