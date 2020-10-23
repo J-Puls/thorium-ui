8cdfef2cb5da186a39108b55e9495d81deae9c5b
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-10-23 16:02:16 -0400

    Remove deprecated code

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -4,7 +4,7 @@
 export const copyToClipboard = (children, setText) => {
   try {
     navigator.clipboard
-      .writeText(children.replace(/~>/g, "\t"))
+      .writeText(children)
       .then(setText("Copied to clipboard!"));
   } catch {
     navigator.clipboard
