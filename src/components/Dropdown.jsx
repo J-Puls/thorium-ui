2e9d900c169a5fdbe6e1b786a0bec4ada62f93bc
J-Puls <54559578+J-Puls@users.noreply.github.com>
2020-10-28 15:52:52 -0400

    Convert to forwardRef
    - Converted Dropdown to forwardRef
    - Provided callback to access menu state when toggled

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,12 @@
 /* React */
-import React, { Children, Component, cloneElement } from "react";
+import React, {
+  Children,
+  forwardRef,
+  cloneElement,
+  useState,
+  useEffect,
+  useRef
+} from "react";
 /* Sub-components */
 import { DropdownDivider as Divider } from "./DropdownDivider";
 import { DropdownItem as Item } from "./DropdownItem";
@@ -37,79 +44,156 @@ const defaultProps = {
   variant: "primary"
 };
 
-/**
- * A collapsible, animated menu with a customizable trigger.
- */
-export class Dropdown extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      active: props.defaultOpen,
-      menuTop: 0
-    };
-    this.toggle = () => {
-      props.triggerType === "click" &&
-        this.setState({ active: !this.state.active });
-    };
-    this.collapse = () => {
-      props.collapseOnClick && props.toggleBurger();
-      this.setState({ active: false });
-    };
-    this.handleMouseEnter = () => {
-      props.triggerType === "hover" && this.setState({ active: true });
-    };
-    this.handleMouseLeave = () => {
-      props.triggerType === "hover" && this.setState({ active: false });
-    };
-  }
-  componentDidMount() {
-    this.setState({ menuTop: this.trigger.offsetHeight });
-  }
-  render() {
-    let children;
-    if (this.props.collapseOnClick) {
-      children = Children.map(this.props.children, (child) => {
-        return cloneElement(child, {
-          onClick: () => this.collapse()
-        });
-      });
+export const Dropdown = forwardRef((props, ref) => {
+  const [active, setActive] = useState(props.defaultOpen);
+  const [menuTop, setMenuTop] = useState(0);
+  useEffect(() => {
+    props.onToggle && props.onToggle(active);
+  }, [active]);
+  
+  const toggle = () => {
+    if (props.triggerType === "click") {
+      setActive(!active);
     }
-    let style;
-    this.props.remoteTrigger && (style = { display: "none" });
+  };
+  const collapse = () => {
+    if (props.collapseOnClick) {
+      props.toggleBurger && props.toggleBurger();
+      setActive(false);
+    }
+  };
+  const handleMouseEnter = () => {
+    props.triggerType === "hover" && setActive(true);
+  };
+  const handleMouseLeave = () => {
+    props.triggerType === "hover" && setActive(false);
+  };
 
-    return (
+  let trigger = useRef();
+  useEffect(() => {
+    setMenuTop(trigger.current.offsetHeight);
+  }, []);
+
+  let children;
+  if (props.collapseOnClick) {
+    children = Children.map(props.children, (child) => {
+      return cloneElement(child, {
+        onClick: () => collapse()
+      });
+    });
+  }
+  let style;
+  props.remoteTrigger && (style = { display: "none" });
+
+  return (
+    <div ref={ref} data-active={active}>
       <Container
-        {...mapPropsToAttrs(this.props)}
-        displayType={this.props.displayType}
-        style={{ marginLeft: 0, marginRight: 0, ...this.props.style }}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
+        {...mapPropsToAttrs(props)}
+        displayType={props.displayType}
+        style={{ marginLeft: 0, marginRight: 0, ...props.style }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <Trigger
-          ref={(el) => (this.trigger = el)}
-          variant={this.props.variant}
-          icons={this.props.icons}
-          active={this.state.active}
-          onClick={this.toggle}
-          text={this.props.text}
-          size={this.props.size}
-          id={this.props.targetID || this.props.id}
+          ref={trigger}
+          variant={props.variant}
+          icons={props.icons}
+          active={active}
+          onClick={toggle}
+          text={props.text}
+          size={props.size}
+          id={props.targetID || props.id}
           style={style}
         />
         <Menu
-          displayType={this.props.displayType}
-          active={this.state.active}
-          height={this.props.height}
-          top={this.state.menuTop}
-          size={this.props.size}
-          scrollable={this.props.scrollable}
+          displayType={props.displayType}
+          active={active}
+          height={props.height}
+          top={menuTop}
+          size={props.size}
+          scrollable={props.scrollable}
         >
-          {children || this.props.children}
+          {children || props.children}
         </Menu>
       </Container>
-    );
-  }
-}
+    </div>
+  );
+});
+
+/**
+ * A collapsible, animated menu with a customizable trigger.
+ */
+// export class Dropdown extends Component {
+//   constructor(props, ref) {
+//     super();
+//     this.ref = ref;
+//     this.state = {
+//       active: props.defaultOpen,
+//       menuTop: 0
+//     };
+//     this.toggle = () => {
+//       props.triggerType === "click" &&
+//         this.setState({ active: !this.state.active });
+//     };
+//     this.collapse = () => {
+//       props.collapseOnClick && props.toggleBurger();
+//       this.setState({ active: false });
+//     };
+//     this.handleMouseEnter = () => {
+//       props.triggerType === "hover" && this.setState({ active: true });
+//     };
+//     this.handleMouseLeave = () => {
+//       props.triggerType === "hover" && this.setState({ active: false });
+//     };
+//   }
+//   componentDidMount() {
+//     this.setState({ menuTop: this.trigger.offsetHeight });
+//   }
+//   render() {
+//     let children;
+//     if (this.props.collapseOnClick) {
+//       children = Children.map(this.props.children, (child) => {
+//         return cloneElement(child, {
+//           onClick: () => this.collapse()
+//         });
+//       });
+//     }
+//     let style;
+//     this.props.remoteTrigger && (style = { display: "none" });
+
+//     return (
+//       <Container
+//         {...mapPropsToAttrs(this.props)}
+//         displayType={this.props.displayType}
+//         style={{ marginLeft: 0, marginRight: 0, ...this.props.style }}
+//         onMouseEnter={this.handleMouseEnter}
+//         onMouseLeave={this.handleMouseLeave}
+//       >
+//         <Trigger
+//           ref={(el) => (this.trigger = el)}
+//           variant={this.props.variant}
+//           icons={this.props.icons}
+//           active={this.state.active}
+//           onClick={this.toggle}
+//           text={this.props.text}
+//           size={this.props.size}
+//           id={this.props.targetID || this.props.id}
+//           style={style}
+//         />
+//         <Menu
+//           displayType={this.props.displayType}
+//           active={this.state.active}
+//           height={this.props.height}
+//           top={this.state.menuTop}
+//           size={this.props.size}
+//           scrollable={this.props.scrollable}
+//         >
+//           {children || this.props.children}
+//         </Menu>
+//       </Container>
+//     );
+//   }
+// }
 
 Dropdown.Divider = Divider;
 Dropdown.Item = Item;
